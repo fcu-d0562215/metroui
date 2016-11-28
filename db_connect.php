e51c7159203db080277c5b05a1185b73240d3714
JoonKui Liew <JoonKui Liew>
2016-11-28 20:41:19 +0800

    copy index.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_USER', "wp-project");
     define('DB_PASSWORD', "wp-project");
-    define('DB_DATABASE', "28.10.2016");
+    define('DB_DATABASE', "wp-project");
     define('DB_SERVER', "ahkui.dynu.com"); 
     $db = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
     if (mysqli_connect_errno()) {
